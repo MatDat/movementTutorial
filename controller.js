9f5df105645a6c9d3bf8d1ae6dbc080b1ee7fd9f
MatDat <math03t7@stud.kea.dk>
2024-04-10 10:04:43 +0200

    first commit

---
+++ controller.js
@@ -0,0 +1,18 @@
+import Model from "./model.js";
+import View from "./view.js";
+
+class Controller {
+  constructor() {
+    this.model = new Model(this);
+    this.view = new View(this);
+  }
+
+  setup() {
+    console.log("JS be running");
+    this.model.testModel();
+    this.view.testView();
+  }
+}
+
+let controller = new Controller();
+window.addEventListener("load", () => controller.setup());
---
+++ model.js
@@ -0,0 +1,7 @@
+export default class Model {
+  constructor() {}
+
+  testModel() {
+    console.log("Model be running");
+  }
+}
